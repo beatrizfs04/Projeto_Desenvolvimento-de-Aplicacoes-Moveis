c025d79dba87276de79232220e18de4b132d9e31
beatrizfs04 <161359918+beatrizfs04@users.noreply.github.com>
2025-05-03 19:18:40 +0100

    Update Gestor.dart

--- lib/Gestor.dart
+++ lib/Gestor.dart
@@ -7,10 +7,6 @@ class Gestao {
 
   Geral DEFINICOES = Geral("Definições");
 
-
-  // VER https://www.macoratti.net/19/11/flut_shapref1.htm
-  // https://medium.flutterdevs.com/using-sharedpreferences-in-flutter-251755f07127
-  // https://pub.dev/packages/shared_preferences
   Future<SharedPreferences> prefs = SharedPreferences.getInstance();
 
   Gestao(this.Nome_APP) {}
